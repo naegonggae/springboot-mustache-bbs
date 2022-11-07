5a7a326452f27d95d4a5f49db3a0c702b26791fa
naegonggae <https://github.com/naegonggae/>
2022-11-08 03:52:51 +0900

    Jpa Repository 만들기까지 완료

---
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -0,0 +1,34 @@
+package com.mustache.bbs.controller;
+
+import com.mustache.bbs.domain.dto.ArticleDto;
+import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.domain.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/articles")
+@Slf4j
+public class ArticleController {
+
+    public final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @GetMapping(value = "/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+    @PostMapping(value = "/posts")
+    public String createArticle(ArticleDto form) {
+        log.info(form.toString());
+        Article article = form.toEntity();
+        articleRepository.save(article);
+        return "";
+    }
+}
